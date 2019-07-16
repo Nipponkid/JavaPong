39f8d21233503786efd2e687daa1234d01addba8
Wesley Brown <nipponkid7@gmail.com>
2019-07-16 13:33:28 -0400

    Initial Commit

---
+++ src/main/java/net/wesleybrown/JavaPong/JavaPong.java
@@ -0,0 +1,52 @@
+package net.wesleybrown.JavaPong;
+
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11C.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+final class JavaPong {
+
+    private long window;
+
+    private JavaPong() {
+        // GLFW has to be initialized
+        if (!glfwInit()) {
+            throw new IllegalStateException("Failed to initialize GLFW");
+        }
+
+        // LWJGL treats GLFWwindow* as long
+        window = glfwCreateWindow(800, 600, "Hello, World!", NULL, NULL);
+        if (window == NULL) {
+            throw new RuntimeException("Failed to create GLFW window");
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+
+        GL.createCapabilities();    // Necessary for LWJGL to work correctly
+    }
+
+    private void loop() {
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+    private void run() {
+        loop();
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    public static void main(String[] args) {
+        final JavaPong javaPong = new JavaPong();
+        javaPong.run();
+    }
+}
